0c331d2d3c407132871e077dcf9acd0dc16328bf
Jessica <andradejsscl@gmail.com>
2023-02-02 11:05:56 -0500

    commit tree c5092a9eedb793d3a222ed7993e10b64b86cd1aa

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total
 
+# initialize needs to take in an argument
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+    end
+
+    def discount
+        @discount
+    end
+
+    def add_item(title, price, quantity = 1)
+        # self.total += price*quantity
+        @total += price * quantity
+
+    end
+
+    def apply_discount
+        @total -= @total * discount / 100
+    end
+
+
+end
+
+cash_register_with_discount = CashRegister.new(20)
+cash_register_with_discount.discount 
+defining instance methods
+need to define discouont
